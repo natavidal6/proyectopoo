fcc3456d91c2ac15d65afda309446e917f51f0ee
nataly vidal realpe <nataly vidal realpe>
2016-11-30 14:50:21 -0500

    Creada carpeta del código del proyecto, con los archivos cpp, main y .h

---
+++ PROYECTO FINAL POO/mercado.h
@@ -0,0 +1,20 @@
+/** 
+@brief Clase principal de industria moderna SA
+*/
+class mercado {
+	public:
+		int depart;
+		double total;
+		
+	
+
+};
+/**
+@brief casa
+*/
+class departamento : public mercado {
+	private:
+		int art;
+		double costo;
+	public:	
+};
